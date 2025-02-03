e0311da35d15557b33d0a005a5011194929c22fd
Logan Ingram <loganphx1@gmail.com>
2025-02-02 23:05:18 -0600

    - Added Formatting to TransactionsList displayed prices

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,7 @@
     <h3>History</h3>
     <ul id="list" class="list">
         <li v-for="transaction in transactions":key="transaction.id":class="transaction.amount < 0 ? 'minus' : 'plus'">
-            {{transaction.text }} <span>${{ transaction.amount }}</span>
+            {{transaction.text }} <span>${{ transaction.amount.toFixed(2) }}</span>
             <button class="delete-btn">x</button>
         </li>
         <!-- <li class="minus">
